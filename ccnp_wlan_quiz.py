604047ed459a1ca7c8661fd9a292caf3920d2de4
DKCisco <dillan.k.craig@gmail.com>
2023-06-25 12:14:59 +0000

    adding questions, changing to allow multiple answers

--- ccnp_wlan_quiz.py
+++ ccnp_wlan_quiz.py
@@ -21,7 +21,7 @@ class Quiz:
                 question, choices, answer, explanation = row
                 self.questions.append(question)
                 self.choices.append(choices.split('|'))
-                self.answers.append(answer)
+                self.answers.append(answer.split('|'))
                 self.explanations.append(explanation)
 
     def shuffle_questions(self):
@@ -29,13 +29,16 @@ class Quiz:
         random.shuffle(combined)
         self.questions, self.choices, self.answers, self.explanations = zip(*combined)
 
-    def check_answer(self, user_answer):
-        if user_answer.lower() == self.answers[self.current_question].lower():
+    def check_answer(self, user_answers):
+        user_answers = [choice_buttons[idx]['text'] for idx in user_answers]
+        correct_answers = self.answers[self.current_question]
+
+        if set(user_answers) == set(correct_answers):
             self.score += 1
             explanation = self.explanations[self.current_question]
             messagebox.showinfo("Correct", f"Your answer is correct!\n\nExplanation:\n{explanation}")
         else:
-            messagebox.showinfo("Incorrect", f"Your answer is incorrect. The correct answer is: {self.answers[self.current_question]}")
+            messagebox.showinfo("Incorrect", f"Your answer is incorrect. The correct answer(s) is/are: {', '.join(correct_answers)}")
         self.current_question += 1
         if self.current_question == len(self.questions):
             self.show_result()
@@ -54,24 +57,22 @@ class Quiz:
         window.destroy()
 
 def submit_answer():
-    selected = selected_choice.get()
-    if selected == -1:
-        messagebox.showinfo("Error", "Please select an answer.")
+    selected = [idx for idx, value in enumerate(selected_choices) if value.get()]
+    if not selected:
+        messagebox.showinfo("Error", "Please select at least one answer.")
     else:
-        user_answer = choice_buttons[selected]['text']
-        quiz.check_answer(user_answer)
+        quiz.check_answer(selected)
 
 def enable_submit_button():
-    selected = selected_choice.get()
-    if selected != -1:
-        submit_button.config(state=tk.NORMAL)  # Enable the submit button when a choice is selected
+    if any(value.get() for value in selected_choices):
+        submit_button['state'] = tk.NORMAL  # Enable the submit button when at least one choice is selected
     else:
-        submit_button.config(state=tk.DISABLED)  # Disable the submit button if no choice is selected
+        submit_button['state'] = tk.DISABLED  # Disable the submit button if no choices are selected
 
 # Create the main window
 window = tk.Tk()
 window.title("Quiz")
-window.geometry("400x300")
+window.geometry("700x400")
 
 # Create question label
 question_label = tk.Label(window, text="Question", wraplength=300)
@@ -79,15 +80,17 @@ question_label.pack(pady=10)
 
 # Create choice buttons
 choice_buttons = []
-selected_choice = tk.IntVar()
+selected_choices = []
 for i in range(4):
-    choice_button = tk.Radiobutton(window, text="", variable=selected_choice, value=i, command=enable_submit_button)
+    selected_choice = tk.BooleanVar()
+    selected_choices.append(selected_choice)
+    choice_button = tk.Checkbutton(window, text="", variable=selected_choice, command=enable_submit_button)
     choice_button.pack(pady=5)
     choice_buttons.append(choice_button)
 
 # Create submit button
 submit_button = tk.Button(window, text="Submit", command=submit_answer, state=tk.DISABLED)
-submit_button.pack(pady=10)
+submit_button.pack(pady=5)
 
 # Load the quiz
 quiz = Quiz("quiz.csv")
